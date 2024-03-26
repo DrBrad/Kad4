38fd375cc74146a93a4f13e3c28cb3dec5dda22b
DrBrad <brad@bradeagle.com>
2024-03-26 13:21:02 -0600

    Kademlia Base change

--- src/unet/kad4/rpc/RequestListener.java
+++ src/unet/kad4/rpc/RequestListener.java
@@ -1,12 +1,13 @@
 package unet.kad4.rpc;
 
 import unet.kad4.Kademlia;
+import unet.kad4.kad.KademliaBase;
 import unet.kad4.kad.Server;
 import unet.kad4.routing.inter.RoutingTable;
 
 public class RequestListener {
 
-    private Kademlia kademlia;
+    private KademliaBase kademlia;
 
     public Server getServer(){
         return kademlia.getServer();
