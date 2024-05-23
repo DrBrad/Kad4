accd535b4d995888c7a5e0376aefbe7f8f5cd484
DrBrad <brad@bradeagle.com>
2024-05-22 19:20:58 -0600

    Changes to test

--- test/unet/kad4/example/Main.java
+++ test/unet/kad4/example/Main.java
@@ -10,18 +10,18 @@ public class Main {
         try{
             Kademlia k = new Kademlia("Kademlia");
             k.getRoutingTable().setSecureOnly(false);
-            k.join(6881, InetAddress.getByName("router.bittorrent.com"), 6881);
-            //k.bind(8070);
-            /*
+            //k.join(6881, InetAddress.getByName("router.bittorrent.com"), 6881);
+            k.bind(8070);
+
             Kademlia k2 = new Kademlia();
             k2.getRoutingTable().setSecureOnly(false);
-            k2.join(8070, InetAddress.getLoopbackAddress(), 8075);
-            */
+            k2.join(8075, InetAddress.getLoopbackAddress(), 8070);
+
 
             while(true){
                 Thread.sleep(10000);
                 System.out.println("CONSENSUS-1: "+k.getRoutingTable().getDerivedUID()+"  "+k.getRoutingTable().getConsensusExternalAddress().getHostAddress()+"  "+k.getRoutingTable().getAllNodes().size());
-                //System.out.println("CONSENSUS-2: "+k2.getRoutingTable().getDerivedUID()+"  "+k2.getRoutingTable().getConsensusExternalAddress().getHostAddress()+"  "+k2.getRoutingTable().getAllNodes().size());
+                System.out.println("CONSENSUS-2: "+k2.getRoutingTable().getDerivedUID()+"  "+k2.getRoutingTable().getConsensusExternalAddress().getHostAddress()+"  "+k2.getRoutingTable().getAllNodes().size());
             }
 
         }catch(Exception e){
